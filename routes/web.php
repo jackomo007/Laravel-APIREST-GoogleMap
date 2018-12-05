e5af30c3fa4136bd78b4b63dc50bcc43c7a777e1
Jose <jeal.code47@gmail.com>
2018-12-05 12:08:55 -0300

    Showing map

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,37 @@
 |
 */
 
+use FarhanWazir\GoogleMaps\GMaps;
+
 Route::get('/', function () {
-    return view('welcome');
+    $config['center'] = '-3.77262142697798, -38.51417962284637';
+    $config['zoom'] = '16';
+    $config['map_height'] = '500px';
+//    $config['map_width'] = '500px';
+    $config['scrollwheel'] = false;
+
+    /*
+     * Initialize GMaps and load the parameters of set
+     */
+    $GMap = new GMaps;
+    $GMap->initialize($config);
+
+    /*
+     * Add Marker
+     */
+    $marker['position'] = '-3.77262142697798, -38.51417962284637';
+    $marker['infowindow_content'] = 'Home sweet home';
+    $GMap->add_marker($marker);
+
+    $marker['position'] = '-3.7708024359243923, -38.5163394855723';
+    $marker['infowindow_content'] = 'Farmacia';
+    $marker['icon'] = 'http://maps.google.com/mapfiles/ms/micons/blue-dot.png';
+    $GMap->add_marker($marker);
+
+    /*
+     * Create the map
+     */
+    $map = $GMap->create_map();
+
+    return view('welcome')->with('map',$map);
 });
