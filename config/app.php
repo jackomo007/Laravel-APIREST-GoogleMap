bfe06a1a77387ddfd4a7a7edfd0bd8dd3a43269e
Jose <jeal.code47@gmail.com>
2018-12-05 09:35:46 -0300

    Setting base configuration

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        FarhanWazir\GoogleMaps\GMapsServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -223,6 +223,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'GMaps' => FarhanWazir\GoogleMaps\Facades\GMapsFacade::class,
 
     ],
 
